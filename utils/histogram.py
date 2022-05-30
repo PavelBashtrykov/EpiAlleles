83e464f6219d8cb00e13bb2dce25a49ce50b03a4
PavelBashtrykov <pavel.bashtrykov@gmail.com>
2022-05-30 22:53:31 +0200

    3rd version

--- utils/histogram.py
+++ utils/histogram.py
@@ -21,8 +21,25 @@ def generate_histogram_kde(data, color="0.8"):
     # sns.despine()
 
 
-def generate_histogram_2sets(data, colors=["b", "g"]):
+def generate_histogram_2sets_1st(data, colors=["b", "g"]):
     sns.set_theme(style="ticks")
     sns.color_palette("Paired")
     sns.histplot(data=data[0], stat="probability", bins=10, color=colors[0])
     sns.histplot(data=data[1], stat="probability", bins=10, color=colors[1])
+
+
+def generate_histogram_2sets(data):
+    # sns.set_theme(style="ticks")
+    # sns.color_palette("Paired")
+    sns.color_palette()
+    sns.histplot(
+        data=data,
+        x="meth_level",
+        hue="sample",
+        bins=10,
+        stat="probability",
+        common_norm=False,
+        # multiple="stack",
+        # kde=True,
+    )
+
--- utils/sam.py
+++ utils/sam.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 METHYLATED_CG_FLAG = 1
 UNMETHYLATED_CG_FLAG = 0
 MISSING_CG_FLAG = "!"
@@ -33,7 +35,7 @@ def calculate_meth_level(meth_pattern):
         return meth_pattern.count(METHYLATED_CG_FLAG) / len(meth_pattern)
 
 
-def get_meth_sam(coordinates, samfile):
+def get_meth_sam2(coordinates, samfile):
     """Extract methylation patterns and methylation levels of individual reads in a sam file."""
     total_read_number = 0
     all_meth_patterns = []
@@ -49,3 +51,25 @@ def get_meth_sam(coordinates, samfile):
             total_read_number += 1
 
     return total_read_number, all_meth_patterns, all_meth_levels
+
+
+def get_meth_sam(coordinates, samfile):
+    """Extract methylation patterns and methylation levels of individual reads in a sam file."""
+    total_read_number = 0
+    all_meth_patterns = []
+    all_meth_levels = []
+    df = pd.DataFrame()
+    with open(samfile, "r") as fh:
+        for i in fh:
+            meth_pattern = analyse_meth_pattern(coordinates, sequence=i.strip())
+
+            meth_level = calculate_meth_level(meth_pattern)
+            if not meth_level:
+                continue
+            all_meth_patterns.append(meth_pattern)
+            all_meth_levels.append(meth_level)
+            total_read_number += 1
+    df["meth_pattern"] = all_meth_patterns
+    df["meth_level"] = all_meth_levels
+    df["sample"] = samfile.strip(".sam")
+    return total_read_number, all_meth_patterns, all_meth_levels, df
