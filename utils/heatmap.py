c1620b387af227b3ae9190f00255c1c79111f795
PavelBashtrykov <pavel.bashtrykov@gmail.com>
2022-06-07 16:21:28 +0200

    heatmap delete unused code

--- utils/heatmap.py
+++ utils/heatmap.py
@@ -1,5 +1,4 @@
 import random
-from tkinter import N
 from typing import Protocol
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -62,42 +61,3 @@ def _generate_heatmap(data: list, xrange: int, color="copper"):
     ax.set_title("Methylation of individual amplicons")
     plt.tight_layout()
     return ax
-
-
-# def get_random_reads_for_heatmap(reads2plot, total_reads_num, patterns):
-#     """Generates a list of random methylation patterns for plotting."""
-#     number = min(reads2plot, total_reads_num)
-
-#     # generate a list of random indices for visualisation
-#     randomlist = []
-#     randomlist = random.sample(range(1, total_reads_num), number)
-
-#     # select the reads based on the list of random indices
-#     selected_reads = []
-#     selected_reads = [patterns[ind] for ind in randomlist]
-#     # sorted_reads = sorted(selected_reads, key=lambda x: x.meth_level, reverse=True)
-#     sorted_reads = sorted(selected_reads, key=lambda x: sum(x), reverse=True)
-#     return sorted_reads
-
-
-# def heatmap(data, color, xrange):
-#     plt.close("all")
-#     fig, ax = plt.subplots(figsize=(9, 6))
-#     sns.heatmap(
-#         data,
-#         annot=False,
-#         xticklabels=np.arange(1,xrange+1,1),
-#         yticklabels=False,
-#         cbar=False,
-#         fmt="d",
-#         cmap=color,
-#         linewidths=0,
-#         ax=ax,
-#     )
-#     return fig
-
-
-# def generate_heatmap(reads2plot, total_reads_num, patterns, color="copper"):
-#     data = _get_random_reads_for_heatmap(reads2plot, total_reads_num, patterns)
-#     xaxisRange = len(patterns[0])
-#     return heatmap(data=data, color=color, xrange=xaxisRange)
